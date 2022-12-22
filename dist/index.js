722610c3c0f230430c5cce342d8b3fa162f270e5
ViniciusCuest <viniciuscostargt@gmail.com>
2022-12-22 17:09:53 -0300

    Added some changes in typescript functions

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,47 @@
 "use strict";
+const buttonSubmit = document.getElementById('submit');
+const buttonBack = document.getElementById('back');
+const buttonNext = document.getElementById('next');
+const textarea = document.getElementById('textarea');
+const couterCharacter = document.getElementById('characters-quantity');
+if (textarea)
+    couterCharacter.innerHTML = String(textarea.maxLength);
+textarea === null || textarea === void 0 ? void 0 : textarea.addEventListener('keyup', () => {
+    couterCharacter.innerHTML = `${textarea.maxLength - textarea.value.length}`;
+    textarea.value = textarea.value.slice(0, textarea.maxLength);
+});
+buttonBack === null || buttonBack === void 0 ? void 0 : buttonBack.addEventListener('click', () => {
+    window.history.back();
+});
+buttonNext === null || buttonNext === void 0 ? void 0 : buttonNext.addEventListener('click', () => {
+    const radio = document.getElementsByName('radio');
+    const checkbox = document.getElementsByName('checkbox');
+    const select = document.getElementById('select-option');
+    if ((textarea === null || textarea === void 0 ? void 0 : textarea.value.length) !== 0) {
+        navigation('submit');
+    }
+    let selectedRadioValue = null;
+    let checkedValues = [];
+    if (radio) {
+        radio.forEach((item) => {
+            if (item.checked)
+                localStorage.setItem('poll', JSON.stringify({ markets: item.value, status: select === null || select === void 0 ? void 0 : select.value }));
+            navigation('poll2');
+            return;
+        });
+    }
+    if (checkbox) {
+        checkbox.forEach((item) => {
+            if (item.checked)
+                localStorage.setItem('poll2', JSON.stringify({ training_resourses: item.value, invest_status: select === null || select === void 0 ? void 0 : select.value }));
+            navigation('details');
+            return;
+        });
+    }
+});
+function submit() {
+    console.log(JSON.parse(String(localStorage.getItem('poll'))));
+}
 function navigation(route) {
     window.location.href = `${route}.html`;
 }
--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,3 +1,59 @@
+const buttonSubmit = document.getElementById('submit')! as HTMLButtonElement | undefined;
+const buttonBack = document.getElementById('back')! as HTMLButtonElement | undefined;
+const buttonNext = document.getElementById('next')! as HTMLButtonElement | undefined;
+
+const textarea = document.getElementById('textarea') as HTMLTextAreaElement | undefined;
+const couterCharacter = document.getElementById('characters-quantity') as HTMLSpanElement;
+
+if(textarea) 
+   couterCharacter.innerHTML = String(textarea.maxLength);
+
+textarea?.addEventListener('keyup', (): void => {
+   couterCharacter.innerHTML = `${textarea.maxLength - textarea.value.length}`;
+   textarea.value = textarea.value.slice(0, textarea.maxLength);
+})
+
+buttonBack?.addEventListener('click', (): void => {
+   window.history.back();
+});
+
+buttonNext?.addEventListener('click', (): void => {
+
+   const radio = document.getElementsByName('radio') as NodeListOf<HTMLInputElement>;
+   const checkbox = document.getElementsByName('checkbox') as NodeListOf<HTMLInputElement>;
+
+   const select = document.getElementById('select-option') as HTMLSelectElement;
+
+   if(textarea?.value.length !== 0) {
+      navigation('submit');
+   }
+
+   let selectedRadioValue: null | string = null;
+   let checkedValues: string[] = [];
+
+   if (radio) {
+      radio.forEach((item: HTMLInputElement) => {
+         if (item.checked)
+            localStorage.setItem('poll', JSON.stringify({ markets: item.value, status: select?.value }))
+         navigation('poll2');
+         return;
+      });
+   }
+
+   if (checkbox) {
+      checkbox.forEach((item: HTMLInputElement) => {
+         if (item.checked)
+            localStorage.setItem('poll2', JSON.stringify({ training_resourses: item.value, invest_status: select?.value }))
+         navigation('details');
+         return;
+      });
+   }
+});
+
+function submit() {
+   console.log(JSON.parse(String(localStorage.getItem('poll'))));
+}
+
 function navigation(route: string): void {
    window.location.href = `${route}.html`;
 }
